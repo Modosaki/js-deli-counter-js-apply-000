e08b438b645f95199b8bf0f17f7f67ebea78c8c9
Karim Ezzat <modosaki@gmail.com>
2018-03-15 15:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function takeANumber(katzDeli,person){
-katzDeli.push(person);
-   return`Welcome, ${person}. You are number ${katzDeli.length} in line.`
+
+
+
+
+function takeANumber(katzDeli){
+katzDeli.push(number);
+   return`You are number ${katzDeli.length} in line.`
 
 }
 
@@ -16,15 +20,27 @@ function nowServing(katzDeli){
 
 
 function currentLine(katzDeli){
+<<<<<<< HEAD
   var text=[]
+=======
+  var text=""
+>>>>>>> 72454d081ad75c6ea232c5dd413f668c564c08a8
   if (katzDeli.length <= 0 ) {return `The line is currently empty.`; 
    }
    
   for (let i= 0; i < katzDeli.length; i++)
  {
+<<<<<<< HEAD
   text.push(` ${i+1}. ${katzDeli[i]}`) 
    
  }
      
       return `The line is currently:${text}`
+=======
+  text.push(`The line is curently: ${i+1}.${katzDeli[i]}`) 
+   
+ }
+     
+      return text;
+>>>>>>> 72454d081ad75c6ea232c5dd413f668c564c08a8
 }
